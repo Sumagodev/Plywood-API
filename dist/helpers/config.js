e1fe4c1d44c7c011be301b2809e41570125af610
unknown <pdalvi0203@gmail.com>
2024-09-14 15:52:50 +0530

    After changes in config

--- dist/helpers/config.js
+++ dist/helpers/config.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CONFIG = void 0;
 require("dotenv/config");
 exports.CONFIG = {
-    MONGOURI: process.env.MONGOURI ? process.env.MONGOURI : "mongodb://admin123:Jzfq2n6b4n15@0.0.0.0:27017/plywood",
-    PORT: process.env.PORT ? process.env.PORT : 3000,
+    MONGOURI: process.env.MONGOURI ? process.env.MONGOURI : "mongodb://0.0.0.0:27017/plywood",
+    PORT: process.env.PORT ? process.env.PORT : 8000,
     JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET ? process.env.JWT_ACCESS_TOKEN_SECRET : "qwertyuiop",
     JWT_ACCESS_REFRESH_TOKEN_SECRET: process.env.JWT_ACCESS_REFRESH_TOKEN_SECRET ? process.env.JWT_ACCESS_REFRESH_TOKEN_SECRET : "qwertyuiopitkgkg",
 };
