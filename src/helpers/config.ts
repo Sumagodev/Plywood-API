73ca26b2c3437050a7a712f76b475b8396657e3b
unknown <pdalvi0203@gmail.com>
2024-09-15 12:36:34 +0530

    changes in UI

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 export const CONFIG = {
-  MONGOURI: process.env.MONGOURI ? process.env.MONGOURI : "mongodb://52.66.150.187:27017/plywood",
+  MONGOURI: process.env.MONGOURI ? process.env.MONGOURI : "mongodb://13.232.176.232:27017/plywood?authSource=plywood",
   PORT: process.env.PORT ? process.env.PORT : 8000,
   JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET ? process.env.JWT_ACCESS_TOKEN_SECRET : "qwertyuiop",
   JWT_ACCESS_REFRESH_TOKEN_SECRET: process.env.JWT_ACCESS_REFRESH_TOKEN_SECRET ? process.env.JWT_ACCESS_REFRESH_TOKEN_SECRET : "qwertyuiopitkgkg",
