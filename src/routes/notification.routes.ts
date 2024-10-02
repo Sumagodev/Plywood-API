bf6b3479080cd310ce26649f19bc6595988d5ac1
Iamthemax <vugore@gmail.com>
2024-10-02 17:19:23 +0530

    After changes in notification controller

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getNotificationsForUser, getUserNotificationCount, updateReadStatus, updateReadStatusNew ,getUserNotificationsController} from "../controllers/notification.controller";
+import { getNotificationsForUser, getUserNotificationCount, updateReadStatus, updateReadStatusNew ,getUserNotificationsController, getUnreadNotificationsCount} from "../controllers/notification.controller";
 
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post("/updateReadStatus", updateReadStatus);
 router.post("/getNotificationCount", getUserNotificationCount);
 router.post("/getAllNotifications", getUserNotificationsController);
 router.post("/upadateRead", updateReadStatusNew);
+router.post("/getUnreadNotificationsCount", getUnreadNotificationsCount);
 
 
 
