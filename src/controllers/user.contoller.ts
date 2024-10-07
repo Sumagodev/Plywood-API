1d8746cb558968e9a1bc929631492376ea6af7ab
2412pooja1 <pdalvi0203@gmail.com>
2024-10-07 16:43:32 +0530

    vrification for phone added in user table

--- src/controllers/user.contoller.ts
+++ src/controllers/user.contoller.ts
@@ -30,14 +30,17 @@ export const addUser = async (req: Request, res: Response, next: NextFunction) =
   try {
 
 
+    console.log(req.body, "Received Request Body");
 
-    // Check if the phone number is verified
+    // Check if the phone number exists and is verified in VerifiedUsers
     const verifiedUser = await VerifiedUsers.findOne({ phone: req.body.phone, status: true });
 
     if (!verifiedUser) {
+      console.log("Phone number not verified or not present in VerifiedUsers");
       return res.status(400).json({ message: "Phone number is not verified", success: false });
     }
 
+    console.log("Phone number verified, proceeding with user creation");
     const documents = [];
     if (req.body.gstCertificate) {
       let gstCertificate = await storeFileAndReturnNameBase64(req.body.gstCertificate);
