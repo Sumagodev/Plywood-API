f9c3ed9e9b1c187bbae06b71f3048b602c965efa
Iamthemax <vugore@gmail.com>
2024-10-15 16:28:59 +0530

    After changes payment controller

--- src/controllers/payments.controller.ts
+++ src/controllers/payments.controller.ts
@@ -66,7 +66,7 @@ export const handleHdfcWebhook = async (req: Request, res: Response, next: NextF
     console.log(decodeBase64(credentials.password))
     console.log(EXPECTED_PASSWORD)
     
-    if (!credentials || decodeBase64(credentials.username) !== EXPECTED_USERNAME || decodeBase64(credentials.password) !== EXPECTED_PASSWORD) {
+    if (decodeBase64(credentials.username) !== EXPECTED_USERNAME || decodeBase64(credentials.password) !== EXPECTED_PASSWORD) {
         return res.status(401).json({ message: 'Unauthorized: Invalid credentials' });
     }
 
